c0b165c7227b01d447bbd92eb3bfa639088a2585
Yurii Sienin <senin.yura99@gmail.com>
2021-03-15 11:01:22 +0200

    create application.properties

---
+++ src/test/java/com/senin/bank_operation/BankOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senin.bank_operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOperationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
